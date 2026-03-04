5c01bc543888b8bd6293ad1a808f84055183fcfb
Aung Phyo Thant <aungphyothant224@gmail.com>
2026-03-04 12:45:39 +0630

    - Configuration for Deployment.

--- src/prisma/prismaClient.ts
+++ src/prisma/prismaClient.ts
@@ -1,10 +1,19 @@
 import "dotenv/config";
+import pg from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client.js";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
-export { prisma };
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
